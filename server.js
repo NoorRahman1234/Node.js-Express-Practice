61f61b6bfa782b6bd36a4f65bc90372be8049f3c
NoorRahman1234 <rahmanafr0123@gmail.com>
2026-02-25 10:35:01 +0500

    Add some comment

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ app.get("/", (req, res) =>{
 app.listen(3000, ()=>{
     console.log(" Hello Noor! Server is running or port 3000");
 });
+
+
+// Output
+// To run this code wirte (node server.js) in terminal it will show "Hello Noor! Server is working on port 3000"
+// then open browser on port 3000 it will show "Hello Noor!! Express is working."
